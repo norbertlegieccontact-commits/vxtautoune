40decb12e839bfd247b3b537721744c8f3d6fa3d
Norbert <norbertelcover@gmail.com>
2026-06-04 16:12:37 +0200

    Fix JuceHeader includes

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -1,5 +1,8 @@
 #pragma once
-#include <JuceHeader.h>
+#include <juce_audio_processors/juce_audio_processors.h>
+#include <juce_audio_utils/juce_audio_utils.h>
+#include <juce_graphics/juce_graphics.h>
+#include <juce_dsp/juce_dsp.h>
 #include "PluginProcessor.h"
 
 // ─────────────────────────────────────────────────────────────────────────────
